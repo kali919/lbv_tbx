89e77bca7d83d7e9f79df7aa381d391506a752f5
kali Mohamed Taha <Kalimohamedtaha@gmail.com>
2024-08-08 15:46:42 +0100

    Initiate commit

--- src/gold/main.py
+++ src/gold/main.py
@@ -3,6 +3,6 @@ from pyspark.sql import SparkSession
 
 def sayhello():
     spark = SparkSession.builder.appName("Test").getOrCreate()
-    print("Hello World")
+    print("Hello World Taha")
     spark.stop()
 
